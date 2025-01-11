b37c611f4082badf787392c64fd357075d20eeec
Taptiyga <48513295+taptiyga@users.noreply.github.com>
2025-01-11 15:45:10 +0400

    script

--- script.js
+++ script.js
@@ -29,25 +29,3 @@ function delTask(e) {
         parentItem.remove()
     }
 }
-// // Добавляем задачу
-// buttonAdd.addEventListener('click', (event) => {
-//     //отменяем перезагрузку страницы
-//     event.preventDefault()
-//     //достаем текст задачи из поля ввода
-//     const inputText = formInput.value
-//     //проверка на наличие текста в поле
-//     if (inputText != "") {
-//         //формирует элемент списка
-//         const inputHTML = `<li class="item">${inputText}</li>`
-//         //добавляем элемент списка и выводим на страницу
-//         list.insertAdjacentHTML('afterbegin', inputHTML)
-//     }
-//     //очищаем поле после добавления
-//     formInput.value = ''
-//     //возвращаем фокус в поле ввода
-//     formInput.focus()
-// })
-// //удаляем все элементы списка
-// buttonDel.addEventListener('click', () => {
-//     list.innerHTML = ""
-// })
